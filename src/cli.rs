16c42016ad035aaaaf82cb3ed7739d1c3023313f
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-02-03 13:58:42 +0530

    cli(to-do): completed the functionlity

---
+++ src/cli.rs
@@ -0,0 +1,26 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(StructOpt, Debug)]
+pub enum Action{
+    Add {
+        #[structopt()]
+        task:String,
+    },
+    Done {
+        #[structopt()]
+        position:usize,
+    },
+    List,
+}
+
+#[derive(Debug,StructOpt)]
+#[structopt(name="Rusty Journal", about="A command line to-do app written in Rust")]
+
+pub struct CommandLineArgs {
+
+    #[structopt(subcommand)]
+    pub action:Action,
+    #[structopt(parse(from_os_str), short,long)]
+    pub journal_file:Option<PathBuf>,
+}
